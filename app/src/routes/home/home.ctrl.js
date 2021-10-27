b8cf24555f8e82bccd42b9f290ac7f4d26ed3156
AEJIJEON <jeonaeji@gist.ac.kr>
2021-10-28 00:39:01 +0900

    회원가입 페이지 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -4,13 +4,16 @@ const User = require("../../models/User");
 const UserStorage = require("../../models/UserStorage");
 
 const output = {
-home : (req, res) => {
-  res.render("home/index");
-},
+  home: (req, res) => {
+    res.render("home/index");
+  },
 
-login : (req, res) => {
-  res.render("home/login");
-}
+  login: (req, res) => {
+    res.render("home/login");
+  },
+  register: (req, res) => {
+    res.render("home/register");
+  },
 };
 const process = {
   login: (req, res) => {
