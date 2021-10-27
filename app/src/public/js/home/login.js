1df821bcc3381d00f5fc9e6efad4b24e68ce4448
AEJIJEON <jeonaeji@gist.ac.kr>
2021-10-27 23:33:21 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -4,20 +4,28 @@ const id = document.querySelector("#id");
 const password = document.querySelector("#password");
 const loginBtn = document.querySelector("button");
 
-loginBtn.addEventListener("click", login);
-
-const login = () => {
-    const req = {
-        id: id.value,
-        password: password.value
-    };
-
+const login = () =>  {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+    console.log(JSON.stringify(req));
     fetch("/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(req)
-    });
-
+        'method': "POST",
+       'headers': {
+    'Content-Type': 'application/json',
+  },
+        'body': JSON.stringify(req),
+    }).then((res) => res.json())
+        .then((res) => {
+            if (res.success) {
+                location.href = "/";
+            } else {
+                alert(res.msg);
+            }
+        }).catch((err) => {
+            console.error(new Error("로그인 중 에러 발생"));
+        });
 };
+
+loginBtn.addEventListener("click", login);
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,12 +1,44 @@
-const home = (req, res) => {
+"use strict";
+
+const output = {
+home : (req, res) => {
   res.render("home/index");
+},
+
+login : (req, res) => {
+  res.render("home/login");
+}
+};
+const users = {
+  id: ["wjsdowl6661", "wjsdowl6662", "wjsdowl6663"],
+  password: ["1", "2", "3"]
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const process = {
+  login: (req, res) => {
+    const id = req.body.id;
+    const password = req.body.password;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true
+        });
+      }
+    
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인 실패"
+    });
+  }
 };
+
+
+
 
 module.exports = {
-    home,
-    login
+    output, process
 };
