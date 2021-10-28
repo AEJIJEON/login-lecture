4d5db92e5bae409586868fbf02ff5014b6c180c2
AEJIJEON <jeonaeji@gist.ac.kr>
2021-10-29 01:38:00 +0900

    AWS RDS 연동 & dotenv로 환경변수 등록

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,16 +9,19 @@ class User{
 
     async login() {
         const client = this.body;
-        const { id, password } = await UserStorage.getUserInfo(client.id);
-        if (id) {
-            if (id === client.id && password === client.password) {
-                return { success: true };
-            }
-            return { success: false, msg: "비번 틀림" };
+        try {
+            const { id, password } = await UserStorage.getUserInfo(client.id);
+            if (id) {
+                if (id === client.id && password === client.password) {
+                    return { success: true };
+                }
+                return { success: false, msg: "비번 틀림" };
 
+            }
+            return { success: false, msg: "없는 아이디" };
+        } catch (err) {
+            return {success: false, msg: err}
         }
-        return { success: false, msg: "없는 아이디" };
-        
     }
 
     async register() {
