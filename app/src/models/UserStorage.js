09a2a46811652d929587e31c20a66d3f397fde93
AEJIJEON <jeonaeji@gist.ac.kr>
2021-10-28 20:49:46 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,44 +1,47 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  static #users = {
-    id: ["wjsdowl6661", "wjsdowl6662", "wjsdowl6663"],
-      password: ["1", "2", "3"],
-    name: ["전1", "전2", "전3"]
-    };
-    
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-            newUsers[field] = users[field];
-    
-            }
-            return newUsers;
-            
-         }, {});
-        return newUsers;
-    }
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {});
 
-    static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) => {
-          newUser[info] = users[info][idx];
-          return newUser;
-        }, {});
+    return userInfo;
+  }
 
-        return userInfo;
-    }
+  static getUsers(...fields) {
+    // const users = this.#users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
+  }
 
-    static save(userInfo) {
-        const users = this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.password.push(userInfo.password);
-        return { success: true };
-    }
+  static getUserInfo(id) {
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+  }
+
+  static save(userInfo) {
+    // const users = this.#users;
+    users.id.push(userInfo.id);
+    users.name.push(userInfo.name);
+    users.password.push(userInfo.password);
+    return { success: true };
+  }
 }
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const User = require("../../models/User");
-const UserStorage = require("../../models/UserStorage");
 
 const output = {
   home: (req, res) => {
@@ -16,9 +15,9 @@ const output = {
   },
 };
 const process = {
-  login: (req, res) => {
+  login: async (req, res) => {
     const user = new User(req.body);
-    const response = user.login();
+    const response = await user.login();
     return res.json(response);
   },
   register: (req, res) => {
